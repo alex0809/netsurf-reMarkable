610f307069640d2a15a09e845a7a62690fa6dd7a
Alex Friesenhahn <11029810+alex0809@users.noreply.github.com>
2021-04-01 19:48:51 +0200

    Add evdev lib to ldflags, add ability to specify local version of libnsfb

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,8 +2,8 @@
 
 SCRIPTPATH=/opt/netsurf/scripts
 if [ ! -f $SCRIPTPATH/env.sh ]; then
-  echo "env.sh doesn't exist, downloading latest..."
-  curl "https://raw.githubusercontent.com/netsurf-browser/netsurf/master/docs/env.sh" -o $SCRIPTPATH/env.sh
+    echo "env.sh doesn't exist, downloading latest..."
+    curl "https://raw.githubusercontent.com/netsurf-browser/netsurf/master/docs/env.sh" -o $SCRIPTPATH/env.sh
 fi
 
 TARGET_WORKSPACE=/opt/netsurf/build
@@ -17,12 +17,25 @@ export LDFLAGS="$LDFLAGS -L$TARGET_WORKSPACE/inst-$HOST/lib"
 # freetype libs end up in /usr/local, so include that for pkg-config
 export PKG_CONFIG_LIBDIR="$PKG_CONFIG_LIBDIR:$SYSROOT/usr/local/lib/pkgconfig"
 
-echo "Cloning fork of libnsfb"
-git clone https://github.com/alex0809/libnsfb-reMarkable.git $TARGET_WORKSPACE/libnsfb
+# For local development: if $LIBNSFB_LOCATION is set, use that instead of checking out from github
+if [ -z ${LIBNSFB_LOCATION+x} ]; then
+    echo "Cloning fork of libnsfb"
+    git clone https://github.com/alex0809/libnsfb-reMarkable.git $TARGET_WORKSPACE/libnsfb
+else
+    echo "LIBNSFB_LOCATION is set, copying into workspace"
+    rm -rf $TARGET_WORKSPACE/libnsfb
+    cp -r $LIBNSFB_LOCATION $TARGET_WORKSPACE/libnsfb
+fi
 
 ns-clone
 ns-pull-install
 
 cd $TARGET_WORKSPACE/netsurf/
+
+# Would probably be nicer to to pkg_config libevdev in the netsurf Makefile,
+# but we are re-pulling that Makefile every build.
+# This works for now.
+export LDFLAGS="$LDFLAGS -levdev"
+
 export BUILD_CC="arm-remarkable-linux-gnueabihf-gcc"
 make TARGET=framebuffer NETSURF_FB_FONTLIB=freetype CC=$BUILD_CC
