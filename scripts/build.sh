65cf3e4b580b6260a3c4ce092e0a62a041964620
Alex Friesenhahn <11029810+alex0809@users.noreply.github.com>
2021-04-05 14:03:27 +0200

    Build from fixed versions

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
 SCRIPTPATH=/opt/netsurf/scripts
-if [ ! -f $SCRIPTPATH/env.sh ]; then
-    echo "env.sh doesn't exist, downloading latest..."
-    curl "https://raw.githubusercontent.com/netsurf-browser/netsurf/master/docs/env.sh" -o $SCRIPTPATH/env.sh
-fi
-
 TARGET_WORKSPACE=/opt/netsurf/build
 HOST=arm-remarkable-linux-gnueabihf
 
+source $SCRIPTPATH/versions.sh
 source $SCRIPTPATH/env.sh
 
 # Required so the netsurf make picks up the previously built libraries
@@ -17,18 +13,11 @@ export LDFLAGS="$LDFLAGS -L$TARGET_WORKSPACE/inst-$HOST/lib"
 # freetype libs end up in /usr/local, so include that for pkg-config
 export PKG_CONFIG_LIBDIR="$PKG_CONFIG_LIBDIR:$SYSROOT/usr/local/lib/pkgconfig"
 
-# For local development: if $LIBNSFB_LOCATION is set, use that instead of checking out from github
-if [ -z ${LIBNSFB_LOCATION+x} ]; then
-    echo "Cloning fork of libnsfb"
-    git clone https://github.com/alex0809/libnsfb-reMarkable.git $TARGET_WORKSPACE/libnsfb
-else
-    echo "LIBNSFB_LOCATION is set, copying into workspace"
-    rm -rf $TARGET_WORKSPACE/libnsfb
-    cp -r $LIBNSFB_LOCATION $TARGET_WORKSPACE/libnsfb
-fi
-
+# For local development, you can clone any repository into target workspace
+# before running this script
 ns-clone
-ns-pull-install
+ns-make-tools install
+ns-make-libs install
 
 cd $TARGET_WORKSPACE/netsurf/
 
