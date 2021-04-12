d25ff7f503e2d099dec27217abb8258ae413d8ed
Alex Friesenhahn <11029810+alex0809@users.noreply.github.com>
2021-04-12 14:16:13 +0200

    Update comment

--- scripts/setup_local_development.sh
+++ scripts/setup_local_development.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set up script for local development.
-# This will prepare clean and then setup the build directory by cloning libnsfb and netsurf-base
+# This will setup the build directory by cloning libnsfb and netsurf-base
 
 git clone https://github.com/alex0809/libnsfb-reMarkable.git ${BUILD_DIR}/libnsfb
 git clone https://github.com/alex0809/netsurf-base-reMarkable.git ${BUILD_DIR}/netsurf
