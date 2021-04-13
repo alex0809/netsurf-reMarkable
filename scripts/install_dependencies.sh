b808d44bfa7f6183aa0877072d66c10e721325b9
Alex Friesenhahn <11029810+alex0809@users.noreply.github.com>
2021-04-13 13:48:10 +0200

    Use the cmake toolchain file from the container instead

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -39,22 +39,6 @@ export DEBIAN_FRONTEND=noninteractive \
     && rm -rf freetype \
     && find "$SYSROOT" -type l,f -name "*.la" | xargs --no-run-if-empty rm
 
-cat > toolchain.cmake <<"EOF"
-set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR arm)
-
-set(CMAKE_SYSROOT /opt/x-tools/arm-remarkable-linux-gnueabihf/arm-remarkable-linux-gnueabihf/sysroot)
-
-set(tools /opt/x-tools/arm-remarkable-linux-gnueabihf)
-set(CMAKE_C_COMPILER ${tools}/bin/arm-linux-gnueabihf-gcc)
-set(CMAKE_CXX_COMPILER ${tools}/bin/arm-linux-gnueabihf-g++)
-
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
-EOF
-
 # Build libjpeg-turbo 2.0.90 targeting armhf
 export DEBIAN_FRONTEND=noninteractive \
     && mkdir libjpeg-turbo \
@@ -64,7 +48,7 @@ export DEBIAN_FRONTEND=noninteractive \
     && sha256sum -c sha256sums \
     && tar --strip-components=1 -xf libjpeg-turbo.tar.gz \
     && rm libjpeg-turbo.tar.gz sha256sums \
-    && cmake -DCMAKE_SYSROOT="$SYSROOT" -DCMAKE_TOOLCHAIN_FILE=../toolchain.cmake -DCMAKE_INSTALL_LIBDIR=$SYSROOT/lib -DCMAKE_INSTALL_INCLUDEDIR=$SYSROOT/usr/include \
+    && cmake -DCMAKE_SYSROOT="$SYSROOT" -DCMAKE_TOOLCHAIN_FILE=/usr/share/cmake/$CHOST.cmake -DCMAKE_INSTALL_LIBDIR=$SYSROOT/lib -DCMAKE_INSTALL_INCLUDEDIR=$SYSROOT/usr/include \
     && make \
     && make install \
     && cd .. \
