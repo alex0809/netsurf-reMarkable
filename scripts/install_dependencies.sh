3eff138213b4cb283e8eb571dbb327b2bc26bb93
Alex Friesenhahn <11029810+alex0809@users.noreply.github.com>
2021-05-11 20:55:42 +0200

    Add missing libiconv build script entry

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,6 +4,19 @@
 # To be run during the Dockerfile build.
 
 # Build libiconv 1.16
+export DEBIAN_FRONTEND=noninteractive \
+    && mkdir libiconv \
+    && cd libiconv \
+    && curl "https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.16.tar.gz" -o libiconv.tar.gz \
+    && echo "e6a1b1b589654277ee790cce3734f07876ac4ccfaecbee8afa0b649cf529cc04  libiconv.tar.gz" > sha256sums \
+    && sha256sum -c sha256sums \
+    && tar --strip-components=1 -xf libiconv.tar.gz \
+    && rm libiconv.tar.gz sha256sums \
+    && ./configure --prefix=$SYSROOT/usr --host="$CHOST" --enable-static --disable-shared \
+    && make \
+    && make install \
+    && cd .. \
+    && rm -rf libiconv || exit 1
 
 # Build openssl 1.1.1k
 export DEBIAN_FRONTEND=noninteractive \
